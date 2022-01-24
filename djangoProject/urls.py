e51ee21ca3f593ce9394aef62255e931ac6d87cb
neto.564 <2100098@aluno.univesp.br>
2022-01-24 14:48:15 -0300

    Add forms

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from todo.models import *
+from todo.forms import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from todo.models import *
+from todo.forms import *
+
 
 def todopage(request):
     tasks = Task.objects.all()
-    context={'tasks':tasks}
+    forms = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+
+    context={'tasks':tasks, 'forms':forms}
     return render(request,'tasks.html', context )
